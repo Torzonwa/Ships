3258b1ce0b0750e44ec042f03282953e6992ec50
William Torzon <torzonwa@grace.edu>
2020-09-09 14:55:38 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ships
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
